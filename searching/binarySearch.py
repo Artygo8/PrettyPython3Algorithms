6a1e0adc7568acb78dd6fb79faa301f1e490d539
Artygo8 <arthur.gossuin@gmail.com>
2021-07-31 18:08:05 +0200

    [IMP] more explicit

--- searching/binarySearch.py
+++ searching/binarySearch.py
@@ -11,9 +11,9 @@ def binarySearch(lst, elem):
 
         if lst[mid] == elem:
             return True
-        elif elem < lst[mid]:
+        if elem < lst[mid]:
             last = mid - 1
-        else:
+        if elem > lst[mid]:
             first = mid + 1
 
     return False
@@ -21,5 +21,5 @@ def binarySearch(lst, elem):
 
 if __name__ == '__main__':
     lst = [2, 1, 7, 0, 6, 5, 8, 4, 3, 9]
-    lst.sort()  # mandatory for biary search
+    lst.sort()  # mandatory for binary search
     print(binarySearch(lst, 2))
